0680293a9f19b1669781dfce7237d7f1325052c6
RosebertBongcao13 <rosebertcolegio143@gmail.com>
2025-09-10 19:19:36 +0800

    Update BongcaoLife.js

--- BongcaoLife.js
+++ BongcaoLife.js
@@ -1,1 +1,38 @@
 
+//Step #1
+let name = "Rosebert Bongcao"
+let course = "Bachelor of Science in Information Technology"
+let year = "2nd Year";
+
+console.log("Name: "+ name);
+console.log("Course: "+ course);
+console.log("Year Level: "+ year);
+
+//Step #2
+let HoursofSleep = 8;
+let HoursofStudy = 2;
+
+console.log("Total hours of sleep: "+ HoursofSleep);
+console.log("Total hours of study: "+ HoursofStudy);
+
+//Step #3
+if (study>5) {
+    console.log("Great! You studied enough today.");
+} else {
+    console.log("You need to study more.");
+}
+
+//Step #4
+let subjects = ["Web System", "Event-Driven","OOP"];
+console.log("List of my subjects this sem:");
+for (let i = 0; i < subjects.length; i++) {
+console.log(subjects [i]);
+}
+
+//Step #5
+function studentSummary() {
+    console.log("My name is "+name+", a "+year+" from "+course);
+    return studentSummary;
+
+}
+console.log(studentSummary());
